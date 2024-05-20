146ca86c1b449c36bc0992ee77dc6df693da00ca
Pham Huynh Phuong Kha <vuamessi9@gmail.com>
2024-05-20 18:28:03 +0700

    feat: rename

--- src/main/java/com/khaphp/configservice/ConfigServiceApplication.java
+++ src/main/java/com/khaphp/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.configservice;
+package com.khaphp.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
